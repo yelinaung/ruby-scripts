3dfba4439137cb346f509e17cd5abd7b3b7bd0b3
yelinaung <yelinaung@yelinaung.(none)>
2013-02-08 18:13:32 +0630

    Added to execute shell command

--- batch.rb
+++ batch.rb
@@ -2,8 +2,10 @@
 def list_file(x)
     Dir.glob("#{x}*.dwg") do |dwg_file|
         next if dwg_file == '.' or dwg_file == '..'
-        puts dwg_file
-    #    return dwg_file
+        puts @dwg = dwg_file
+        #dir = "#{Dir.home}/libredwg/examples/dwg2svg"
+        #y = system("sh #{dir}")
     end
 end
+
 list_file(ARGV[0].to_s)
