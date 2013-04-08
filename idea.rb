f3f31e8fcd941993a387fa06a5922c840c0d8b01
mgpyone <mgpyone@mysteryzillion.com>
2013-04-08 20:12:30 +0630

    Modified the idea.rb

--- idea.rb
+++ idea.rb
@@ -2,32 +2,55 @@ require 'nokogiri'
 require 'json'
 
 class Currencies
-    def initialize(page, bank)
+    @bank
+    def initialize (page, bank)
         @page = page
         @bank = bank
     end
 
     def parsed
-        @parsed ||= @page.css("tr")[1..3].collect{|el| el.text.gsub(/\s+/,'') }
+        @parsed ||= @page.css("tr")[1..5].collect{|el| el.text.gsub(/\s+/,'') }
     end
 
-    def to_hash
+    def to_hash_AGD
     {
         bank: @bank,
         rates: {
             USD: usd,
             SGD: sgd,
+            EURO: euro,
+        }
+    }
+    end
+
+    def to_hash_CB
+    {
+        bank: @bank,
+        rates: {
+            USD: usd,
+            FEC: fec,
+            SGD: sgd,
+            EURO: euro,
         }
     }
     end
 
     def usd
-        parsed[0].gsub(/^USD/, '')
+        parsed[0].gsub(/[^0-9]|s+/, '')
+    end
+
+    def fec
+        parsed[1].gsub(/^[0-9]|s+/,'')
     end
 
     def sgd
-        parsed[1].gsub(/^SGD/, '')
+        parsed[2].gsub(/[^0-9]|s+/, '')
+    end
+
+    def euro
+        parsed[3].gsub(/[^0-9]|s+/, '')
     end
 end
 
-puts Currencies.new(Nokogiri::HTML(open("AGD.html")),"AGD").to_hash.to_json
+puts Currencies.new(Nokogiri::HTML(open("AGD.html")),"AGD").to_hash_AGD.to_json
+puts Currencies.new(Nokogiri::HTML(open("CB.html")),"CB").to_hash_CB.to_json
--- noko.rb
+++ noko.rb
@@ -10,17 +10,27 @@ sources = {
     central: "http://forex.cbm.gov.mm/api/latest"
 }
 
-hash
 #puts sources[:cb]
 puts "Currencies from CB Bank are"
 if @page = Nokogiri::HTML(open("AGD.html"))
     (1..3).each do |i|
-        puts @page.css("tr")[i].text.gsub(/\s+/,'')
+        #puts @page.css("tr")[i].text.gsub(/\s+/,'')
     end
 end
-#puts "Currencies from AGD Bank are"
-if @page = Nokogiri::HTML(open("CB.html"))
-    (1..3).each do |x|
-        #puts @page.css("tr")[x].text
-    end
+
+def to_hash
+{
+    rates: {
+        USD: usd,
+    }
+}
 end
+
+def usd
+    @page.css("tr")[1..3].text.gsub(/\s+|USD/,'')
+end
+
+def sgd
+end
+
+puts @doc= Nokogiri::HTML(open("AGD.html")).to_json
