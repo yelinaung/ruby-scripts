4b5585de4b381cb220ee075451f40d29c98ceffb
mgpyone <mgpyone@mysteryzillion.com>
2013-04-10 20:22:51 +0630

    Split the cur into array

--- idea.rb
+++ idea.rb
@@ -1,6 +1,14 @@
 require 'nokogiri'
 require 'json'
+require 'open-uri'
+require 'net/http'
 
+sources = {
+    cb: "http://www.cbbankmm.com/FXRatesHo.php",
+    kbz: "http://www.kbzbank.com/",
+    agd: "http://www.agdbank.com/category/exchange-rate/",
+    central: "http://forex.cbm.gov.mm/api/latest"
+}
 class Currencies
     @bank
     def initialize (page, bank)
@@ -9,7 +17,7 @@ class Currencies
     end
 
     def parsed
-        @parsed ||= @page.css("tr")[1..5].collect{|el| el.text.gsub(/\s+/,'') }
+        @parsed ||= @page.css("tr")[1..3].collect{|el| el.text.gsub(/\s+/,'') }
     end
 
     def to_hash_AGD
@@ -36,21 +44,21 @@ class Currencies
     end
 
     def usd
-        parsed[0].gsub(/[^0-9]|s+/, '')
+       parsed[0].gsub(/[^0-9]|s+/,'').to_s.scan(/.../).map { |h| h.to_i}.to_s
     end
 
-    def fec
-        parsed[1].gsub(/FEC|^[0-9]|s+/,'')
-    end
+    #def fec
+    #    parsed[1].gsub(/FEC|^[0-9]|s+/,'')
+    #end
 
     def sgd
-        parsed[2].gsub(/[^0-9]|s+/, '')
+        parsed[1].gsub(/[^0-9]|s+/,'').to_s.scan(/.../).map { |h| h.to_i}.to_s
     end
 
     def euro
-        parsed[3].gsub(/[^0-9]|s+/, '')
+        parsed[2].gsub(/[^0-9]|s+/,'').to_s.scan(/..../).map { |h| h.to_i}.to_s
     end
 end
 
 puts Currencies.new(Nokogiri::HTML(open("AGD.html")),"AGD").to_hash_AGD.to_json
-puts Currencies.new(Nokogiri::HTML(open("CB.html")),"CB").to_hash_CB.to_json
+#puts Currencies.new(Nokogiri::HTML(open("CB.html")),"CB").to_hash_CB.to_json
