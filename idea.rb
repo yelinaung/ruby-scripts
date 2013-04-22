a8b54dd8db6371c3636b5313d60dde6a9c7dfe97
mgpyone <mgpyone@mysteryzillion.com>
2013-04-22 18:22:44 +0630

    Added kbz and sin_cur

--- idea.rb
+++ idea.rb
@@ -11,6 +11,55 @@ sources = {
     central: "http://forex.cbm.gov.mm/api/latest"
 }
 
+class KBZ
+  @doc = Nokogiri::HTML(open("KBZ.html"))
+
+  # Selecting the first col
+  x = @doc.css("tr")[1].text.gsub(/\n/,' ')
+  # Splitting the string into array
+  buy = x.chomp.split(/\s+/)
+  @usd_b = buy[1]
+  @sgd_b = buy[2]
+  @euro_b = buy[3]
+  @fec_b = buy[4]
+
+  # Selecting the second col
+  y = @doc.css("tr")[2].text.gsub(/\n/,' ')
+  # Splitting array into array
+  sell = y.chomp.split(/\s+/)
+  @usd_s = sell[1]
+  @sgd_s = sell[2]
+  @euro_s = sell[3]
+  @fec_s = sell[4]
+
+  def to_hash_KBZ
+  {
+      bank: "KBZ",
+      rates: {
+          USD:KBZ.kbz_usd,
+          SGD:KBZ.kbz_sgd,
+          EURO:KBZ.kbz_euro,
+          FEC:KBZ.kbz_fec
+      }
+  }
+  end
+
+  def KBZ.kbz_usd
+      "[#{@usd_b},#{@usd_s}]"
+  end
+
+  def KBZ.kbz_sgd
+      "[#{@sgd_b},#{@sgd_s}]"
+  end
+
+  def KBZ.kbz_euro
+      "[#{@euro_b},#{@euro_b}]"
+  end
+
+  def KBZ.kbz_fec
+      "[#{@fec_b},#{@fec_s}]"
+  end
+end
 
 class Currencies
     def initialize (page, bank)
@@ -47,19 +96,6 @@ class Currencies
             FEC: cb_fec,
             SGD: cb_sgd,
             EURO: euro,
-        }
-    }
-    end
-
-    # To hash method for KBZ
-    def to_hash_KBZ
-    {
-        bank: @bank,
-        rates: {
-            USD: usd,
-            #SGD: sgd,
-            #EUR: kbz_euro,
-            #FEC: kbz_fec,
         }
     }
     end
@@ -102,6 +138,6 @@ class Currencies
     end
 end
 
-puts Currencies.new(Nokogiri::HTML(open(sources[:agd])),"AGD").to_hash_AGD.to_json
-puts Currencies.new(Nokogiri::HTML(open(sources[:cb])),"CB").to_hash_CB.to_json
-puts Currencies.new(Nokogiri::HTML(open(sources[:kbz])),"KBZ").to_hash_KBZ.to_json
+puts JSON.pretty_generate(Currencies.new(Nokogiri::HTML(open(sources[:agd])),"AGD").to_hash_AGD)
+puts JSON.pretty_generate(Currencies.new(Nokogiri::HTML(open(sources[:cb])),"CB").to_hash_CB)
+puts JSON.pretty_generate(KBZ.new.to_hash_KBZ)
--- kbz.rb
+++ kbz.rb
@@ -3,8 +3,23 @@ require 'json'
 
 @doc = Nokogiri::HTML(open("KBZ.html"))
 
-puts @doc.css("td")[0].text.gsub(/\n/,',')
-puts @doc.css("tr").text
+# Selecting the first col
+x = @doc.css("tr")[1].text.gsub(/\n/,' ')
+# Splitting the string into array
+buy = x.chomp.split(/\s+/)
+@usd_b = buy[1]
+@sgd_b = buy[2]
+@euro_b = buy[3]
+@fec_b = buy[4]
+
+# Selecting the second col
+y = @doc.css("tr")[2].text.gsub(/\n/,' ')
+# Splitting array into array
+sell = y.chomp.split(/\s+/)
+@usd_s = sell[1]
+@sgd_s = sell[2]
+@euro_s = sell[3]
+@fec_s = sell[4]
 
 def to_hash
 {
@@ -13,7 +28,25 @@ def to_hash
         USD:usd,
         SGD:sgd,
         EURO:euro,
-        FEC:fec,
+        FEC:fec
     }
 }
 end
+
+def usd
+    "[#{@usd_b},#{@usd_s}]"
+end
+
+def sgd
+    "[#{@sgd_b},#{@sgd_s}]"
+end
+
+def euro
+    "[#{@euro_b},#{@euro_b}]"
+end
+
+def fec
+    "[#{@fec_b},#{@fec_s}]"
+end
+
+puts JSON.pretty_generate(to_hash)
